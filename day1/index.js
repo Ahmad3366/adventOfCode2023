cc80f47c343b663d79ff95dd386efde602a6ab63
Ahmed <ahmaddx900@gmail.com>
2024-07-28 08:26:44 +0200

    refactor: add intermediate variable

--- day1/index.js
+++ day1/index.js
@@ -17,8 +17,9 @@ function lineNums(line) {
 
 function calcColNums(input) {
 	let total = 0;
+	const lines = input.split('\n')
 
-	for (const line of input.split('\n')) {
+	for (const line of lines) {
 		const linenum = lineNums(line);
 		total += Number(linenum[0] + linenum[linenum.length - 1])
 	}
